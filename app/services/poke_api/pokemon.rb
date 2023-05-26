f55ed8e63405c91136fd8ff5fd5390af84f3d870
Abraham <abkuri88@gmail.com>
2023-05-26 23:49:20 +0000

    Adds pokemon class with abilities and moves mapping

---
+++ app/services/poke_api/pokemon.rb
@@ -0,0 +1,69 @@
+module PokeApi
+  class Pokemon
+    attr_reader :id, :name, :height, :weight, :abilities, :moves
+
+    # Map almost every attribute from the API to a local attribute
+    def initialize(attributes)
+      @id = attributes[:id]
+      @name = attributes[:name]
+      @height = attributes[:height]
+      @weight = attributes[:weight]
+      @abilities = Pokemon::Abilities.new(attributes[:abilities])
+      @moves = Pokemon::Moves.new(attributes[:moves])
+    end
+
+    def self.find(id)
+      response = PokeApi::Request.get("/pokemon/#{id}")
+
+      new(response.to_json)
+    end
+
+    class Ability
+      def initialize(attributes = {})
+        @attributes = attributes
+      end
+
+      def hidden?
+        @attributes[:is_hidden]
+      end
+
+      def slot
+        @slot ||= @attributes[:slot]
+      end
+
+      def name
+        @name ||= @attributes[:ability][:name]
+      end
+
+      def url
+        @url ||= @attributes[:ability][:url]
+      end
+    end
+
+    class Abilities < Array
+      def initialize(abilities = {})
+        super(abilities.map { |ability| Pokemon::Ability.new(ability) })
+      end
+    end
+
+    class Move
+      def initialize(attributes = {})
+        @attributes = attributes
+      end
+
+      def name
+        @name ||= @attributes[:move][:name]
+      end
+
+      def url
+        @url ||= @attributes[:move][:url]
+      end
+    end
+
+    class Moves < Array
+      def initialize(moves = {})
+        super(moves.map { |move| Pokemon::Move.new(move) })
+      end
+    end
+  end
+end
---
+++ spec/services/poke_api/pokemon_ability_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe PokeApi::Pokemon::Ability do
+  describe '#is_hidden?' do
+    context 'when the ability is not hidden' do
+      it 'returns false' do
+        attributes[:is_hidden] = false
+
+        ability = described_class.new(attributes)
+
+        expect(ability).not_to be_hidden
+      end
+    end
+    context 'when the ability is hidden' do
+      it 'returns true' do
+        attributes[:is_hidden] = true
+
+        ability = described_class.new(attributes)
+
+        expect(ability).to be_hidden
+      end
+    end
+  end
+
+  describe '#slot' do
+    it 'returns the slot' do
+      attributes = { slot: 1 }
+
+      ability = described_class.new(attributes)
+
+      expect(ability.slot).to eq(1)
+    end
+  end
+
+  describe '#name' do
+    it 'returns the name' do
+      attributes = { ability: { name: 'overgrow' } }
+
+      ability = described_class.new(attributes)
+
+      expect(ability.name).to eq('overgrow')
+    end
+  end
+
+  describe '#url' do
+    it 'returns the url' do
+      attributes = { ability: { url: 'https://pokeapi.co/api/v2/ability/65/' } }
+
+      ability = described_class.new(attributes)
+
+      expect(ability.url).to eq('https://pokeapi.co/api/v2/ability/65/')
+    end
+  end
+end
